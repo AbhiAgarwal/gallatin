cab860e4b143ed48d5cc72ad540a017f50f1dc06
Abhi Agarwal <me@abhiagarwal.com>
2015-05-01 01:06:52 -0400

    Removing unncessary files

--- Classes/Moral Behavior: Sentiment & Psychology/PaperFinal/Tech@NYU Intranet_files/filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('app.filters', []).
-  filter('interpolate', function (version) {
-    return function (text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  });
--- Classes/Moral Behavior: Sentiment & Psychology/PaperFinal/Tech@NYU Intranet_files/main-ctrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular
-.module('app.controllers')
-.controller('MainCtrl', function($scope, apiDescriptor) {
-	apiDescriptor.then(function(apiDescription) {
-		$scope.apidesc = apiDescription.data;
-	});
-});
